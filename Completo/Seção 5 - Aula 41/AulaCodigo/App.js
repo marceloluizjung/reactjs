8a487480843c9f0e55b961f1f48b9d5a2306850d
Marcelo <marcelo_4940@hotmail.com>
2023-03-09 21:22:17 -0300

    vai que vai

---
+++ Completo/Seção 5 - Aula 41/AulaCodigo/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Feed from './components/Feed';
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            feed:[
+                {id: 1, username: 'Matheus', curtidas:10, comentarios:2},
+                {id: 2, username: 'Lucas', curtidas:120, comentarios: 24},
+                {id: 3, username: 'Amanda', curtidas:30, comentarios: 12},
+                {id: 4, username: 'Ricardo', curtidas:1, comentarios: 0},
+            ]
+        };
+
+    }
+
+    render(){
+        return(
+            <div>
+
+                {this.state.feed.map((item)=>{
+                    return(
+                        <Feed id={item.id} username={item.username}
+                              curtidas={item.curtidas} comentarios={item.comentarios}/>
+                    );
+                })}
+
+            </div>         
+        );
+    }
+}
+
+export default App;
